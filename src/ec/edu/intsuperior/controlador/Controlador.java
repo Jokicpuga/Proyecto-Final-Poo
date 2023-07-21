aa83d6322ab59a6ba23404f933657541ecf6af09
Jhonatan <Jhonatan@DESKTOP-ST21IOU>
2023-07-20 21:36:15 -0500

    Ejercicio programacion version 1.1

---
+++ src/ec/edu/intsuperior/controlador/Controlador.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.controlador;
+
+import ec.edu.intsuperior.vista.Inicio;
+import ec.edu.intsuperior.vista.Loggin;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class Controlador {
+    Loggin loggin= new Loggin(this);
+    Inicio inicio= new Inicio(this);
+    
+    public void mostrarInicio(){
+        inicio.setVisible(true);
+    }
+    
+    public void mostrarLoggin(){
+        loggin.setVisible(true);
+    }
+      public void ocultarLoggin(){
+        loggin.setVisible(false);
+    }
+       public void ocultarInicio(){
+        inicio.setVisible(false);
+    }
+    
+    
+}
